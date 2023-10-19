b062185200f8fb2137f77d4afb96d5657d649ab4
Kurt Stohrer <kurt@stohrer.net>
2023-10-19 14:26:40 -0400

    updates

--- src/install-antenna-runner-ctl.sh
+++ src/install-antenna-runner-ctl.sh
@@ -1,8 +1,9 @@
 # Step 1: Download the script using curl
-curl -O https://github.com/kurtstohrer/antenna-runner-ctl/blob/main/src/antenna-runner-ctl.py
+curl -O https://raw.githubusercontent.com/kurtstohrer/antenna-runner-ctl/main/src/antenna-runner-ctl.py
+
 
 # Step 2: Rename the script to 'antenna-runner-ctl'
-mv antenna-runner.py antenna-runner-ctl
+mv antenna-runner-ctl.py antenna-runner-ctl
 
 # Step 3: Provide execute permissions
 chmod +x antenna-runner-ctl
